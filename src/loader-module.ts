7f34b6dfedec528d747facc81bd45337973248f7
Apteryx <apteryx.xyz@gmail.com>
2024-06-19 14:41:54 +1200

    Remove the no longer needed internal exports

--- src/loader-module.ts
+++ src/loader-module.ts
@@ -4,6 +4,3 @@ export * from './types'
 export * from './posthog-surveys-types'
 export const posthog = init_as_module()
 export default posthog
-
-import { autocapture } from './autocapture'
-export const internal = { autocapture }
